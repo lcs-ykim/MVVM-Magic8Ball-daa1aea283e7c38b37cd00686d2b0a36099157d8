53bffae2b4315b51ba8cdac0cd95c9f64f221e51
Yeseo Kim <YKim@lcs.on.ca>
2021-09-19 22:41:53 -0400

    Initial Commit

---
+++ iOS/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by Yeseo Kim on 2021-09-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Yeseo Kim on 2021-09-19.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
